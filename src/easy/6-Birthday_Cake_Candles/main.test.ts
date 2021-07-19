045e29eebf4df6150ecababe4cb2e43bf21cfc3b
danilo <danilobandeira29@gmail.com>
2021-07-19 19:56:39 -0300

    fix: method name

--- src/easy/6-Birthday_Cake_Candles/main.test.ts
+++ src/easy/6-Birthday_Cake_Candles/main.test.ts
@@ -3,5 +3,5 @@ import Cake from "./main";
 test("should return the count of the tallest candle", function () {
     const candles = [3, 2, 1, 3];
     const cake = new Cake(candles);
-    expect(cake.countCandles()).toBe(2);
+    expect(cake.countTallestCandle()).toBe(2);
 });

e0f9f673990905438f19769d120ad4c6323c2c4b
danilo <danilobandeira29@gmail.com>
2021-07-20 20:36:01 -0300

    style: change test description

--- src/easy/8-Grading_Students/grade.test.ts
+++ src/easy/8-Grading_Students/grade.test.ts
@@ -6,7 +6,7 @@ test("should not round grade if value is less than 38", function () {
     expect(grade.value).toBe(37);
 });
 
-test("should round grade if value is greater or equal to 38", function () {
+test("should round grade if value is greater than or equal to 38", function () {
     const number = 38;
     const grade = new Grade(number);
     expect(grade.value).toBe(40);

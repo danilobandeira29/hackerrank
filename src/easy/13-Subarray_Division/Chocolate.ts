29e2c7f4e1f67288d2b1aa98cd21d3f613f7519f
danilo <danilobandeira29@gmail.com>
2021-07-25 15:58:08 -0300

    feat: add Chocolate

---
+++ src/easy/13-Subarray_Division/Chocolate.ts
@@ -0,0 +1,31 @@
+export default class Chocolate {
+    selected: number[];
+    birthDay: number;
+    birthMonth: number;
+
+    constructor (selected: number[], birthDay: number, birthMonth: number) {
+        this.selected = selected;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+    }
+
+    divide (): number {
+        return this.selected.reduce((accumulator, _, index) => {
+            const numbers = this.segment(index);
+            if(this.isValidToDivide(numbers)) accumulator++;
+            return accumulator;
+        }, 0)
+    }
+
+    segment (number: number) {
+        return this.selected.slice(number, number + this.birthMonth);;
+    }
+
+    isValidToDivide(numbers: number[]) {
+       return this.sumNumbers(numbers) === this.birthDay;
+    }
+
+    sumNumbers (numbers: number[]) {
+        return numbers.reduce((acc, curr) => acc+curr, 0);
+    }
+}

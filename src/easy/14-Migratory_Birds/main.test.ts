4eed652fb46a9985246f43da075ddc046fca79da
danilo <danilobandeira29@gmail.com>
2021-07-26 18:39:21 -0300

    test: should return the lowest type id of the most frequently sighted birds

---
+++ src/easy/14-Migratory_Birds/main.test.ts
@@ -0,0 +1,14 @@
+import Birds from "./Birds";
+
+test("should return 4 because is the lowest type id of the most frequently sighted birds", function () {
+    const ids = [1, 4, 4, 4, 5, 3];
+    const lowestIdMostRepeated = new Birds(ids).lowestIdMostRepeated();
+    expect(lowestIdMostRepeated).toBe(4); 
+});
+
+test("should return 3 because the lowest type id of the most frequently sighted birds", function () {
+    const ids = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4];
+    const lowestIdMostRepeated = new Birds(ids).lowestIdMostRepeated();
+    expect(lowestIdMostRepeated).toBe(3); 
+});
+

e75172f28d2808f3812f0afde7f1c000a95d05a2
danilo <danilobandeira29@gmail.com>
2021-07-28 21:18:02 -0300

    test: should count pairs of stocks with matching colors

---
+++ src/easy/16-Sales_By_Match/main.test.ts
@@ -0,0 +1,13 @@
+import Socks from "./Socks";
+
+test("should count pairs of stocks with matching colors", function () {
+    const socks = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+    const count = new Socks(socks.length, socks).countPairs();
+    expect(count).toBe(3);
+});
+
+test("should count pairs of stocks with matching colors", function () {
+    const socks = [1, 1, 3, 1, 2, 1, 3, 3, 3, 3];
+    const count = new Socks(socks.length, socks).countPairs();
+    expect(count).toBe(4);
+});

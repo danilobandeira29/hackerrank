a6988e5ec60fac15e8dd05a73191cd079d6cafe4
danilo <danilobandeira29@gmail.com>
2021-08-01 11:23:36 -0300

    refactor: add template method to CalendarWithLeapYear

--- src/easy/18-Day_Of_The_Programmer/CalendarWithLeapYear.ts
+++ src/easy/18-Day_Of_The_Programmer/CalendarWithLeapYear.ts
@@ -1,9 +1,22 @@
 import Calendar from "./Calendar";
 
 export default abstract class CalendarWithLeapYear extends Calendar {
+    firstEightMonthsInDays: number[];
+
     constructor (year: number) {
         super(year);
+        this.firstEightMonthsInDays = [31, 28, 31, 30, 31, 30, 31, 31];
     }
 
     abstract isLeapYear(): boolean;
+
+    sumDaysEightFirstMonths() {
+        const sumDaysEightFirstMonths = this.firstEightMonthsInDays.reduce((accumulator, current) => accumulator += current, 0);
+        return this.isLeapYear() ? sumDaysEightFirstMonths + 1 : sumDaysEightFirstMonths;
+    }
+
+    day256thOfTheYear () {
+        if(256 - this.sumDaysEightFirstMonths() === 13) return `13.09.${this.year}`;
+        return `12.09.${this.year}`;
+    }
 }
--- src/easy/18-Day_Of_The_Programmer/GregorianCalendar.ts
+++ src/easy/18-Day_Of_The_Programmer/GregorianCalendar.ts
@@ -1,27 +1,13 @@
 import CalendarWithLeapYear from "./CalendarWithLeapYear";
 
 export default class GregorianCalendar extends CalendarWithLeapYear {
-    monthsInDays: number[];
-
     constructor (year: number) {
         if(year <= 1918) throw new Error("Year not supported by Gregorian's Calendar");
         super(year);
-        this.monthsInDays = [31, this.februaryDays(), 31, 30, 31, 30, 31, 31];
-    }
-
-    februaryDays () {
-        if(this.isLeapYear()) return 29;
-        return 28;
     }
 
     isLeapYear () {
         if((this.year % 4 == 0 && this.year % 100 != 0 ) || (this.year % 400 == 0)) return true;
         return false;
-    }
-
-    day256thOfTheYear () {
-        const sumDaysEightFirstMonths = this.monthsInDays.reduce((accumulator, current) => accumulator += current, 0);
-        if(256 - sumDaysEightFirstMonths === 13) return `13.09.${this.year}`;
-        return `12.09.${this.year}`;
     }
 }

575783ad32a693d46febad16dac9741e5d2cea72
danilo <danilobandeira29@gmail.com>
2021-08-08 17:33:03 -0300

    refactor: add meaningful context

--- src/easy/5-Min-Max_Sum/Combination.test.ts
+++ src/easy/5-Min-Max_Sum/Combination.test.ts
@@ -1,23 +1,23 @@
-import MinMaxSum from "./main";
+import Combination from "./Combination";
 
 test("should find the minimum and maximum values summing exactly four of the five integers", function () {
     const values = [1, 2, 3, 4, 5];
     console.log = jest.fn();
-    const minMaxSum = new MinMaxSum(values);
-    minMaxSum.calculate();
+    const combination = new Combination(values);
+    combination.printMinAndMax();
     expect(console.log).toHaveBeenCalledWith("10 14");
 });
 
 test("should find the minimum and maximum values summing exactly four of the five integers", function () {
     const values = [7, 69, 2, 221, 8974];
     console.log = jest.fn();
-    const minMaxSum = new MinMaxSum(values);
-    minMaxSum.calculate();
+    const combination = new Combination(values);
+    combination.printMinAndMax()
     expect(console.log).toHaveBeenCalledWith("299 9271");
 });
 
 
 test("should throw if values length is 0", function () {
-    expect(() => new MinMaxSum([] as number[])).toThrow(new Error("Invalid values length."));
+    expect(() => new Combination([] as number[])).toThrow(new Error("Invalid values length."));
 });
 

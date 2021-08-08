35127f695bb4ef2514f0a114f50ead87260ba397
danilo <danilobandeira29@gmail.com>
2021-08-08 16:49:21 -0300

    refactor: add meaningful context

--- src/easy/3-Plus_Minus/Number.test.ts
+++ src/easy/3-Plus_Minus/Number.test.ts
@@ -1,10 +1,9 @@
-import PlusMinus from "./main";
+import Number from "./Number";
 
 test("should print lines with the proportion of integers, negative and zero values contained in an array", function () {
     const values = [-4, 3, -9, 0, 4, 1];
     console.log = jest.fn();
-    const plusMinus = new PlusMinus();
-    plusMinus.execute(values);
+    Number.proportions(values);
     expect(console.log).toHaveBeenNthCalledWith(1, 0.500000);
     expect(console.log).toHaveBeenNthCalledWith(2, 0.333333);
     expect(console.log).toHaveBeenNthCalledWith(3, 0.166667);

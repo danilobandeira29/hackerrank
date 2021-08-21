4b769012855393006fa711edc2e93f31b8e54f2d
danilo <danilobandeira29@gmail.com>
2021-08-21 16:06:57 -0300

    refactor: change variable name

--- src/easy/14-Migratory_Birds/MigratoryBirds.test.ts
+++ src/easy/14-Migratory_Birds/MigratoryBirds.test.ts
@@ -2,13 +2,13 @@ import MigratoryBirds from "./MigratoryBirds";
 
 test("should return 4 because is the lowest type id of the most frequently sighted birds", function () {
     const ids = [1, 4, 4, 4, 5, 3];
-    const lowestIdMostRepeated = new MigratoryBirds(ids).getLowestBirdIdWithHighestCount();
-    expect(lowestIdMostRepeated).toBe(4); 
+    const lowestIdWithHighestCount = new MigratoryBirds(ids).getLowestBirdIdWithHighestCount();
+    expect(lowestIdWithHighestCount).toBe(4);
 });
 
 test("should return 3 because the lowest type id of the most frequently sighted birds", function () {
     const ids = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4];
-    const lowestIdMostRepeated = new MigratoryBirds(ids).getLowestBirdIdWithHighestCount();
-    expect(lowestIdMostRepeated).toBe(3); 
+    const lowestIdWithHighestCount = new MigratoryBirds(ids).getLowestBirdIdWithHighestCount();
+    expect(lowestIdWithHighestCount).toBe(3);
 });
 

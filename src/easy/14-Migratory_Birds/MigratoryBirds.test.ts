4371464b8a84f7bb6f277057d7ff637ebdde8d0e
danilo <danilobandeira29@gmail.com>
2021-08-21 16:04:26 -0300

    refactor: clean code

--- src/easy/14-Migratory_Birds/MigratoryBirds.test.ts
+++ src/easy/14-Migratory_Birds/MigratoryBirds.test.ts
@@ -1,14 +1,14 @@
-import Birds from "./Birds";
+import MigratoryBirds from "./MigratoryBirds";
 
 test("should return 4 because is the lowest type id of the most frequently sighted birds", function () {
     const ids = [1, 4, 4, 4, 5, 3];
-    const lowestIdMostRepeated = new Birds(ids).lowestIdMostRepeated();
+    const lowestIdMostRepeated = new MigratoryBirds(ids).getLowestBirdIdWithHighestCount();
     expect(lowestIdMostRepeated).toBe(4); 
 });
 
 test("should return 3 because the lowest type id of the most frequently sighted birds", function () {
     const ids = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4];
-    const lowestIdMostRepeated = new Birds(ids).lowestIdMostRepeated();
+    const lowestIdMostRepeated = new MigratoryBirds(ids).getLowestBirdIdWithHighestCount();
     expect(lowestIdMostRepeated).toBe(3); 
 });
 

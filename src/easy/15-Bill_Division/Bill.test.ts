efd58a401dc1746950f113f542e86ef0f66f11e9
danilo <danilobandeira29@gmail.com>
2021-08-22 16:18:39 -0300

    refactor: clean code

--- src/easy/15-Bill_Division/Bill.test.ts
+++ src/easy/15-Bill_Division/Bill.test.ts
@@ -1,20 +1,20 @@
-import BonAppetit from "./BonAppetit";
+import Bill from "./Bill";
 
 test("should print 5 because it's the money that Brian have to refund Anna", function () {
-    const bill = [3, 10, 2, 9];
-    const annaDidNotEat = 1;
+    const itemsEaten = [3, 10, 2, 9];
+    const itemEatenForOnePerson = 1;
     const moneyPayed = 12;
     console.log = jest.fn();
-    new BonAppetit(bill, annaDidNotEat, moneyPayed).calculateBill();
+    new Bill(itemsEaten, itemEatenForOnePerson, moneyPayed).calculate();
     const refundAnna = 5;
     expect(console.log).toHaveBeenNthCalledWith(1, refundAnna);
 });
 
 test("should print Bon Appetit because Brian does not have to refund Anna", function () {
-    const bill = [3, 10, 2, 9];
-    const annaDidNotEat = 1;
+    const itemsEaten = [3, 10, 2, 9];
+    const itemEatenForOnePerson = 1;
     const moneyPayed = 7;
     console.log = jest.fn();
-    new BonAppetit(bill, annaDidNotEat, moneyPayed).calculateBill();
+    new Bill(itemsEaten, itemEatenForOnePerson, moneyPayed).calculate();
     expect(console.log).toHaveBeenNthCalledWith(1, "Bon Appetit");
 });

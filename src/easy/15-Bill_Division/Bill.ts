929a65793f43427cc05d0be56ed966bd4205cffe
danilo <danilobandeira29@gmail.com>
2021-08-22 16:22:24 -0300

    refactor: change variable name

--- src/easy/15-Bill_Division/Bill.ts
+++ src/easy/15-Bill_Division/Bill.ts
@@ -1,14 +1,14 @@
 export default class Bill {
     private readonly itemsEaten: number[];
     private readonly moneyPayed: number;
-    private total: number | string;
+    private refund: number | string;
     private itemsBothEaten: number[];
 
     constructor(itemsEaten: number[], itemEatenForOnePerson: number, moneyPayed: number) {
         this.itemsEaten = itemsEaten;
         this.itemsBothEaten = this.calculateItemsBothEaten(itemEatenForOnePerson);
         this.moneyPayed = moneyPayed;
-        this.total = 0;
+        this.refund = 0;
     }
 
     private calculateItemsBothEaten(itemEatenForOnePerson: number) {
@@ -16,8 +16,8 @@ export default class Bill {
     }
 
     calculate() {
-        const totalEachPersonWillPay = this.calculateTotalEachPersonWillPay();
-        this.calculateRefund(totalEachPersonWillPay);
+        const refundEachPersonWillPay = this.calculateTotalEachPersonWillPay();
+        this.calculateRefund(refundEachPersonWillPay);
         this.print();
     }
 
@@ -25,11 +25,11 @@ export default class Bill {
         return this.itemsBothEaten.reduce((accumulator, current) => accumulator += current, 0) / 2;
     }
 
-    private calculateRefund(totalEachPersonWillPay: number) {
-        this.total = Math.abs(totalEachPersonWillPay - this.moneyPayed) || "Bon Appetit";
+    private calculateRefund(refundEachPersonWillPay: number) {
+        this.refund = Math.abs(refundEachPersonWillPay - this.moneyPayed) || "Bon Appetit";
     }
 
     private print() {
-        console.log(this.total);
+        console.log(this.refund);
     }
 }
